3f3cd2ecb81410792f2ddb189339ed6352d8fd1f
dazamora <dazamoraa@unal.edu.co>
2016-09-26 16:37:09 -0500

    asa

---
+++ R/inicio.R
@@ -0,0 +1,16 @@
+.onLoad <- function(libname, pkgname) {
+  op <- options()
+  op.devtools <- list(
+    devtools.path = "~/R-dev",
+    devtools.install.args = "",
+    devtools.name = "David Zamora",
+    devtools.desc.author = '"David Zamora <dazamoraa@unal.edu.co> [aut, cre]"',
+    devtools.desc.license = "What license is it under?",
+    devtools.desc.suggests = NULL,
+    devtools.desc = list()
+  )
+  toset <- !(names(op.devtools) %in% names(op))
+  if(any(toset)) options(op.devtools[toset])
+
+  invisible()
+}
